d302037db1283866397d30aec4ad230d52a87699
Bestnogm1 <koffiarielhessou@gmail.com>
2022-02-12 14:43:13 -0500

    added the math.random to the js

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ const item2 = document.querySelector('.item2')
 const item3 = document.querySelector('.item3')
 const playButton = document.querySelector('.play_button')
 
-button.addEventListener('click',()=>{
+playButton.addEventListener('click',()=>{
+  //picking the math.randome element
+  let randomNUmber = math.floor(Math.random()*4) + 1
+  let randomNUmber2 = math.floor(Math.random()*4) + 1
+  let randomNUmber3 = math.floor(Math.random()*4) + 1
+  //this random number picks a number bettewn 1 and4 
   
 })
