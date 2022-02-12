29b84643a63b433acb228ce8c920540d29f438d7
Bestnogm1 <koffiarielhessou@gmail.com>
2022-02-12 14:32:47 -0500

    added all the selector in the js file

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
+const message = document.querySelector('.messages')
 const item1 = document.querySelector('.item1')
+const item2 = document.querySelector('.item2')
+const item3 = document.querySelector('.item3')
+const playButton = document.querySelector('.play_button')
+
+button.addEventListener('click',()=>{
+  
+})
