85dca309f6e46791b5db3c50ddf454f00de75aa5
Bestnogm1 <koffiarielhessou@gmail.com>
2022-02-12 16:42:58 -0500

    working proiject

--- app.js
+++ app.js
@@ -1,24 +1,28 @@
-const message = document.getElementsByClassName('messages')
-const firstNumber =document.getElementsByClassName('item1')
-const secondNumber =document.getElementsByClassName('item2')
-const thirdNumber = document.getElementsByClassName('item3')
+const message = document.querySelector('.messages')
+const firstNumber =document.querySelector('.item1')
+const secondNumber =document.querySelector('.item2')
+const thirdNumber = document.querySelector('.item3')
 console.log(firstNumber);
 let playButton = document.querySelector('.play_button' )
 console.log(playButton);
+
 playButton.addEventListener('click',function(){
   //picking the math.randome element
-  let randomNumber = math.floor(Math.random()*4) + 1;
-  let randomNumber2 = math.floor(Math.random()*4) + 1;
-  let randomNumber3 = math.floor(Math.random()*4) + 1;
+  let randomNumber = Math.floor(Math.random()*4) + 1;
+  let randomNumber2 = Math.floor(Math.random()*4) + 1;
+  let randomNumber3 = Math.floor(Math.random()*4) + 1;
   //this random number picks a number bettewn 1 and4 
-  let firstNumber1 = item1.innerHTML = `${randomNumber}`;
-  let secondNumber2 = item2.innerHTML = `${randomNumber2}`;
-  let thirdNumber3 = item3.innerHTML = `${randomNumber3}`;
+  let firstNumber1 = firstNumber.innerHTML = `${randomNumber}`;
+
+  let secondNumber2 = secondNumber.innerHTML = `${randomNumber2}`;
+
+  let thirdNumber3 = thirdNumber.innerHTML = `${randomNumber3}`;
+
   let lose = 'you have lost';
   let win = 'you have won!';
   if(firstNumber1 === secondNumber2 && firstNumber1 === thirdNumber3){
-    return `${win}` 
+    message.innerHTML=`${win}` 
   } else {
-    return `${lose}`
+    message.innerHTML =`${lose}`
   }
 })
