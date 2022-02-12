aa222658f7d09a3b46190ab09d3e9caa24562aa7
Bestnogm1 <koffiarielhessou@gmail.com>
2022-02-12 15:09:06 -0500

    added the inner html for the display

--- app.js
+++ app.js
@@ -6,9 +6,14 @@ const playButton = document.querySelector('.play_button')
 
 playButton.addEventListener('click',()=>{
   //picking the math.randome element
-  let randomNUmber = math.floor(Math.random()*4) + 1
-  let randomNUmber2 = math.floor(Math.random()*4) + 1
-  let randomNUmber3 = math.floor(Math.random()*4) + 1
+  let randomNumber = math.floor(Math.random()*4) + 1;
+  let randomNumber2 = math.floor(Math.random()*4) + 1;
+  let randomNumber3 = math.floor(Math.random()*4) + 1;
   //this random number picks a number bettewn 1 and4 
-  
+  let firstNumber = item1.innerHTML = `${randomNumber}`;
+  let secondNumber = item2.innerHTML = `${randomNumber2}`;
+  let thirdNumber = item3.innerHTML = `${randomNumber3}`;
+  let lose = 'you have lost';
+  let win = 'you have win!';
+
 })
