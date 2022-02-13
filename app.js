d9c55dbdec892382ce6bec0424e911a4bdb2f64b
Bestnogm1 <koffiarielhessou@gmail.com>
2022-02-13 17:13:48 -0500

    add setTimeout

--- app.js
+++ app.js
@@ -8,32 +8,19 @@ function ranNumber(){
   //This random number picks a number bettewn 1 and4 
   return  Math.floor(Math.random()*4) + 1 
 } 
-setTimeout(ranNumber, 30000);
-console.log('After setTimeout');
 
 playButton.addEventListener('click',()=>{
   //picking the math.randome element
   let randomNumber = ranNumber()
   let randomNumber2 = ranNumber()
   let randomNumber3 = ranNumber()
-  
-  let firstNumber = item1.innerHTML = `${randomNumber}`
-
-  let secondNumber = item2.innerHTML = `${randomNumber2}`
-
-  let thirdNumber = item3.innerHTML = `${randomNumber3}`
-
+  let firstNumber = setTimeout(()=>{item1.innerHTML = `${randomNumber}`},1000);
+  let secondNumber = setTimeout(()=>{item2.innerHTML = `${randomNumber2}`},2000)
+  let thirdNumber = setTimeout(()=>{item3.innerHTML = `${randomNumber3}`},3000) 
   let lose = 'you have lost';
-
   let win = 'you have won!';
-
   if(firstNumber === secondNumber && firstNumber === thirdNumber){
-
-    message.innerHTML=`${win}` 
-
+    setTimeout(()=>{ message.innerHTML=`${win}` }, 3000)  
   } else {
-
-    message.innerHTML =`${lose}`}
-
-
+    setTimeout(()=>{ message.innerHTML =`${lose}`} , 3500) }
 })
