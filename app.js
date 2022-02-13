b4f7306b106e831c33816a63505808802c47b10a
Bestnogm1 <koffiarielhessou@gmail.com>
2022-02-13 15:40:25 -0500

    still working

--- app.js
+++ app.js
@@ -5,27 +5,34 @@ const item3 = document.querySelector('.item3')
 const playButton = document.querySelector('.play_button')
 
 function ranNumber(){
-   return  Math.floor(Math.random()*4) + 1 
-    
+  //This random number picks a number bettewn 1 and4 
+  return  Math.floor(Math.random()*4) + 1 
 } 
+setTimeout(ranNumber, 30000);
+console.log('After setTimeout');
 
 playButton.addEventListener('click',()=>{
   //picking the math.randome element
   let randomNumber = ranNumber()
-  let randomNumber2 = Math.floor(Math.random()*4) + 1
-  let randomNumber3 = Math.floor(Math.random()*4) + 1
-  //This random number picks a number bettewn 1 and4 
+  let randomNumber2 = ranNumber()
+  let randomNumber3 = ranNumber()
+  
   let firstNumber = item1.innerHTML = `${randomNumber}`
+
   let secondNumber = item2.innerHTML = `${randomNumber2}`
+
   let thirdNumber = item3.innerHTML = `${randomNumber3}`
 
   let lose = 'you have lost';
+
   let win = 'you have won!';
+
   if(firstNumber === secondNumber && firstNumber === thirdNumber){
 
     message.innerHTML=`${win}` 
+
   } else {
-    
+
     message.innerHTML =`${lose}`}
 
 
